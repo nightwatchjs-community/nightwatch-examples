395a8e652950174aac9c9ee1f27091c1cf5de1df
Andrei Rusu <andrei.rusu@beatfactor.net>
2022-05-27 21:11:00 +0200

    added folders

---
+++ tests/googlePageObject.js
@@ -0,0 +1,41 @@
+describe('google search with consent form - page objects', function() {
+
+  this.disabled = this.argv.env !== 'chrome';
+
+  const homePage = browser.page.google.search();
+  const consentPage = browser.page.google.consent();
+
+  before(async () => homePage.navigate());
+
+  after(async (browser) => browser.quit());
+
+  it('should complete the consent form', async function (browser) {
+
+    const consentPresent = await homePage.isPresent('@consentModal');
+
+    if (consentPresent) {
+      await homePage.expect.section('@consentModal').to.be.visible;
+
+      const {consentModal} = homePage.section;
+      await consentModal.click('@customizeButton');
+
+      await browser.expect.url().toContain('https://consent.google.');
+      await consentPage.turnOffEverything();
+    }
+  });
+
+  it('should find nightwatch.js in results', function (browser) {
+    homePage.setValue('@searchBar', 'Nightwatch.js');
+    homePage.submit();
+
+    const resultsPage = browser.page.google.searchResults();
+    resultsPage.expect.element('@results').to.be.present;
+
+    resultsPage.expect.element('@results').text.to.contain('Nightwatch.js');
+
+    resultsPage.expect.section('@menu').to.be.visible;
+
+    const menuSection = resultsPage.section.menu;
+    menuSection.expect.element('@all').to.be.visible;
+  });
+});
